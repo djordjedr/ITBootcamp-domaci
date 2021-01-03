49c90b2ce9ae3d969211808d02423d2baf2d6639
djordjedr <68948897+djordjedr@users.noreply.github.com>
2021-01-03 20:30:16 +0100

    Add files via upload

---
+++ DrugiZadatak1.java
@@ -0,0 +1,25 @@
+package projekatZaOdmor;
+
+public class DrugiZadatak1 {
+
+	public static void main(String[] args) {
+		// Zadatak sa tehnickog intervjua: ako je x = 3, a y = 7, Napisati program tako da im se vrednosti zamene. Dostaviti oba resenja, sa i bez uvodjenja novih promenljivih
+		
+		int x=3;
+		int y=7;
+		int z=0;
+		 z=x;	
+		 
+		 x=y;	
+		
+		 y=z;
+		
+		 System.out.println("x je; "+x);
+		 
+		 System.out.println("y je; "+y);
+		
+		
+		
+	}
+
+}
